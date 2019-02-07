7bcfaf2c8afaa9d45c2387e1b5ebc769352e8e57
libido <leonardovlibido@gmail.com>
2019-02-07 21:23:25 +0100

    add gcov out parsing, calculate line cov percent

--- src/build_n_run.py
+++ src/build_n_run.py
@@ -1,6 +1,8 @@
 import subprocess
 import os
-print("hi")
+import re
+
+#print("hi")
 
 def bash(command):
     arr = command.split()
@@ -8,13 +10,38 @@ def bash(command):
     try:
         res = subprocess.check_output(arr)
     except BaseException as error:
-        print('An exception occurred: {}'.format(error))
+        print('error, an exception occurred: {}'.format(error))
     if res is None:
         return
-    print("command '"+command+"' result:")
+    #print("command '"+command+"' result:")
 
-    for line in res.splitlines():
-        print("\t",line)
+    #for line in res.splitlines():
+        #print("\t",line)
+
+    return res
+
+def coverage_percent(gcov_out):
+    gcov_out = gcov_out.decode("utf-8")
+    lines_covered = 0
+    all_lines = 0
+
+    for block in gcov_out.split("\n\n"):
+        func_out = block.splitlines()
+
+        if len(func_out) < 2:
+            continue
+
+        # TODO: if curr function is not in test project pass
+        #if projectName not in func_out[1]:
+        #    pass
+
+        if "Lines executed" in func_out[1]:
+            func_info = re.findall(r"[-+]?\d*\.\d+|\d+", func_out[1])
+            func_info = [float(i) for i in func_info]
+            lines_covered += round(0.01*func_info[0]*func_info[1])
+            all_lines += func_info[1]
+
+    return lines_covered/all_lines
 
 folder = "what_we_test"
 os.chdir(folder)
@@ -29,7 +56,7 @@ bash("ls")
 try:
     os.makedirs(folder)
 except OSError as e:
-    print(e)
+    print("error " + e)
 
 os.chdir(folder)
 bash("pwd")
@@ -41,7 +68,11 @@ bash("qmake "+projPath)
 bash("make -B")
 fileName = "tst_"+projName 
 bash("./"+fileName)
-bash("gcov "+fileName+".cpp")
+#gcov_out = bash("gcov "+fileName+".cpp")
+
+gcov_out = bash("gcov string_distance.cpp")
+cvg_percent = coverage_percent(gcov_out)
+print("%.6f" % cvg_percent)
 
 
-print("bye")
+#print("bye")
