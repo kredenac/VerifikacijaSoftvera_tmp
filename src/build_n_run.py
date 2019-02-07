e5b9414dd19365abcab016987ab632fcdf6ae16a
kredenac <zacementirano@gmail.com>
2019-02-07 18:08:58 +0100

    script to build,run and exec tests

--- src/build_n_run.py
+++ src/build_n_run.py
@@ -4,21 +4,44 @@ print("hi")
 
 def bash(command):
     arr = command.split()
-    res = subprocess.check_output(arr)
+    res = None
+    try:
+        res = subprocess.check_output(arr)
+    except BaseException as error:
+        print('An exception occurred: {}'.format(error))
+    if res is None:
+        return
+    print("command '"+command+"' result:")
+
     for line in res.splitlines():
-        print("fromPy:",line)
+        print("\t",line)
 
 folder = "what_we_test"
 os.chdir(folder)
+
 folder = "build-cov"
+#remove the folder and contents
+bash("pwd")
+bash("ls")
+bash("rm -rf "+folder)
+bash("ls")
+
 try:
     os.makedirs(folder)
 except OSError as e:
     print(e)
 
 os.chdir(folder)
+bash("pwd")
 
-bash("ls")
-bash("rm *")
-bash("ls")
+projName = "string_distance_test"
+projPath = "../Root/" + projName + "/" + projName + ".pro"
 
+bash("qmake "+projPath)
+bash("make -B")
+fileName = "tst_"+projName 
+bash("./"+fileName)
+bash("gcov "+fileName+".cpp")
+
+
+print("bye")
